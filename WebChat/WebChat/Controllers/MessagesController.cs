ae21e9eac6a2bdb0d7be44ff3ff059e9e1772168
Michael F <storm7222@gmail.com>
2017-12-11 17:42:01 +0200

    Version 1

---
+++ WebChat/WebChat/Controllers/MessagesController.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using WebChat.Data;
+using WebChat.Models;
+using Microsoft.AspNetCore.Identity;
+using WebChat.Services;
+using Microsoft.Extensions.Logging;
+
+namespace WebChat.Controllers
+{
+    public class MessagesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public MessagesController(ApplicationDbContext context, UserManager<ApplicationUser> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+
+        [HttpPost]
+        //Own
+        public async Task<IActionResult> Index([Bind("Id,Text,Author")] Message message)
+        {
+            if (ModelState.IsValid)
+            {
+                message.Date = DateTime.Now;
+
+                _context.Add(message);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(message);
+        }
+
+        //Own
+        public async Task<IActionResult> Index()
+        {
+            var user = await _userManager.GetUserAsync(User);
+            if (user != null)
+            {
+                ViewBag.Sign = user.ApplicationUserSign;
+            }
+            return View(_context.Message.ToList().OrderBy(t => t.Date));
+        }
+
+        //// GET: Messages
+        //public async Task<IActionResult> Index()
+        //{
+        //    return View(await _context.Message.ToListAsync());
+        //}
+
+        // GET: Messages/Details/5
+        public async Task<IActionResult> Details(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var message = await _context.Message
+                .SingleOrDefaultAsync(m => m.Id == id);
+            if (message == null)
+            {
+                return NotFound();
+            }
+
+            return View(message);
+        }
+
+        // GET: Messages/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Messages/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Text")] Message message)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(message);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(message);
+        }
+
+        // GET: Messages/Edit/5
+        public async Task<IActionResult> Edit(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var message = await _context.Message.SingleOrDefaultAsync(m => m.Id == id);
+            if (message == null)
+            {
+                return NotFound();
+            }
+            return View(message);
+        }
+
+        // POST: Messages/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(string id, [Bind("Id,Text,Date")] Message message)
+        {
+            if (id != message.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(message);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!MessageExists(message.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(message);
+        }
+
+        // GET: Messages/Delete/5
+        public async Task<IActionResult> Delete(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var message = await _context.Message
+                .SingleOrDefaultAsync(m => m.Id == id);
+            if (message == null)
+            {
+                return NotFound();
+            }
+
+            return View(message);
+        }
+
+        // POST: Messages/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(string id)
+        {
+            var message = await _context.Message.SingleOrDefaultAsync(m => m.Id == id);
+            _context.Message.Remove(message);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool MessageExists(string id)
+        {
+            return _context.Message.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ WebChat/WebChat/Models/ManageViewModels/IndexViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebChat.Models.ManageViewModels
+{
+    public class IndexViewModel
+    {
+        public string Username { get; set; }
+
+        public bool IsEmailConfirmed { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        public string StatusMessage { get; set; }
+
+        [Display(Name = "Sign")]
+        public string IndexViewModelSign { get; set; }
+    }
+}
